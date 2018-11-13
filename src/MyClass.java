3b0db68e94fe52db70f821110254a2e5f372511e
Eve Lin <evelin.dicheva@gmail.com>
2018-11-13 10:32:00 +0200

    Get & Set

---
+++ src/MyClass.java
@@ -0,0 +1,9 @@
+public class MyClass {
+
+    public static void main(String[] args) {
+        GetterAndSetter person = new GetterAndSetter();
+        person.setName("John");
+        System.out.println(person.getName());
+    }
+
+}
